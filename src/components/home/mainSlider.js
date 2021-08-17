1ee09fc60c08e91440953c0a2cb817d63e2ff9f5
exit <esraayousef400@gmail.com>
2021-08-17 03:02:05 +0200

    styling

---
+++ src/components/home/mainSlider.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/swiper.scss";
+import "swiper/components/navigation/navigation.scss";
+
+const MainSlider = () => {
+  const [slider, setSlider] = useState(null);
+
+  useEffect(() => {
+    getData();
+    async function getData() {
+      const response = await fetch("http://localhost:8000/api/v1/categories/");
+      const data = await response.json();
+      setSlider(data);
+    }
+  }, []);
+
+  return (
+    <div className="home-slider-parallax">
+      {/* display books from the API */}
+      {slider && (
+        <Swiper
+          spaceBetween={20}
+          speed={600}
+          navigation={true}
+          className="mySwiper"
+        >
+          {slider.map((slide, index) => (
+            <SwiperSlide key={index}>
+              <img src={slide.image} alt={"home-Slide-" + index} />
+              <div className="parallax-text">
+                <h1>{slide.title}</h1>
+                <h3>{slide.description}</h3>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      )}
+    </div>
+  );
+};
+
+export default MainSlider;
