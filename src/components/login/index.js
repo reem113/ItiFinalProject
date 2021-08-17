04084c4ebb211b55568be40a9e76e38c586d23c6
Reem Shokry <reemshokry0@gmail.com>
2021-08-17 21:32:58 +0200

    login updated

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from 'react'
 import Logo from "../../images/logo_company.svg";
 import { Link } from "react-router-dom";
 
@@ -6,53 +6,70 @@ import { Link } from "react-router-dom";
 
 import { AiOutlineLock, AiOutlineUser } from "react-icons/ai";
 
-class Login extends React.Component {
-    render() {
-        return (
+function Login() {
 
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
 
-            <>
-                <div class="form-container ">
-                    <div class="d-flex justify-content-center">
-                        <img src={Logo} alt="Logo" />
+    function loginUser(e) {
+        e.preventDefault()
+
+        fetch('http://localhost:3000/api/v1/users/login', {
+            method: 'POST', // or 'PUT'
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ email, password }),
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Success:', data);
+                if (!data.success) console.log("---------------------")
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    }
+
+    return (
+        <>
+            <div class="form-container">
+                <div class="d-flex justify-content-center">
+                    <img src={Logo} alt="Logo" />
+                </div>
+                <form method="POST" onSubmit={(event) => { loginUser(event) }}>
+                    <div class="input-group mb-3">
+                        <span class="input-group-text p-4"><AiOutlineUser /></span>
+                        <input type="email" name="email" class="form-control" onChange={(event) => { setEmail(event.target.value) }} placeholder="E-mail" />
                     </div>
-                    <form>
-                        <div class="input-group mb-3">
-                            <span class="input-group-text p-4"><AiOutlineUser /></span>
-                            <input type="text" name="username" class="form-control" value="" onChange={() => { }} placeholder="Username" />
-                        </div>
 
-                        <div class="input-group mb-2">
-                            <span class="input-group-text p-4"><AiOutlineLock /></span>
-                            <input type="password" name="password" class="form-control" value="" onChange={() => { }} placeholder="Password" />
-                        </div>
+                    <div class="input-group mb-2">
+                        <span class="input-group-text p-4"><AiOutlineLock /></span>
+                        <input type="password" name="password" class="form-control" onChange={(event) => { setPassword(event.target.value) }} placeholder="Password" />
+                    </div>
 
-                        <div class="form-group">
-                            <div class="custom-control custom-checkbox">
-                                <input type="checkbox" class="custom-control-input p-2" id="rememberCheckBox" />
-                                <label class="custom-control-label m-2 form-text" for="rememberCheckBox">Remember me</label>
-                            </div>
+                    <div class="form-group">
+                        <div class="custom-control custom-checkbox">
+                            <input type="checkbox" class="custom-control-input p-2" id="rememberCheckBox" />
+                            <label class="custom-control-label m-2 form-text" for="rememberCheckBox">Remember me</label>
                         </div>
+                    </div>
 
-                        <div class="d-flex justify-content-center ">
-                            <Link className="btn-main" to="/">Login</Link>
-                        </div>
-                    </form>
-                    <div class="mt-4">
-                        <div class="d-flex justify-content-center links form-text ">
-                            Don't have an account? <Link class="ml-2 signup-link" to="/signup"><u>Sign Up</u></Link>
-                        </div>
-                        <div class="d-flex justify-content-center links form-text">
-                            <a href="#">Forgot your password?</a>
-                        </div>
+                    <div class="d-flex justify-content-center ">
+                        <input type="submit" value="Login" className="btn btn-main" />
+                    </div>
+                </form>
+                <div class="mt-4">
+                    <div class="d-flex justify-content-center links form-text ">
+                        Don't have an account? <Link class="ml-2 signup-link" to="/signup"><u>Sign Up</u></Link>
+                    </div>
+                    <div class="d-flex justify-content-center links form-text">
+                        <Link href="#">Forgot your password?</Link>
                     </div>
                 </div>
-
-
-            </>
-
-        );
-    }
+            </div>
+        </>
+    )
 }
 
-export default Login;
+export default Login
